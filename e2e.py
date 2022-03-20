42d44940a0a645ba08a02c1c9461d6bf4e7a32d9
izchaki <izchaki10@gmail.com>
2022-03-20 15:52:45 +0200

    adding dockerfile,selenium file(e2e.py) and Scores.txt(as db) for flask-server container

---
+++ e2e.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import os
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+chromeOptions = webdriver.ChromeOptions()
+chromeOptions.add_argument("--no-sandbox")
+chromeOptions.add_argument("--disable-dev-shm-using")
+chromeOptions.add_argument("--disable-extensions")
+chromeOptions.add_argument(r"user-data-dir=.\cookies\\test")
+chromeOptions.headless = True
+chrome_driver = webdriver.Chrome(chrome_options=chromeOptions)
+
+
+def test_scores_service(url):
+    try:
+        chrome_driver.get(url)
+        scores = chrome_driver.find_element(By.XPATH, value='//*[@id="score"]').text
+        print(scores)
+        chrome_driver.quit()
+        return 1 < int(scores) < 1000
+    except Exception as e:
+        print(e)
+
+
+def main_function():
+    if test_scores_service('http://localhost:5001') is True:
+        return 0
+    return -1
+
+
+print(main_function())
