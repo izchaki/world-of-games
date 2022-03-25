421ae37e84245a95f61e14d3e2bf48046f59303f
izchaki <88430606+izchaki@users.noreply.github.com>
2022-03-25 14:02:07 +0300

    pipeline for my first jenkins

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,24 @@
+pipeline {
+    agent { label 'build image' }
+    stages {
+        stage('Build docker image'){
+            steps{
+                sh 'docker build -t izchaki/my-flask:from-jenkins-pipeline .'
+                sh 'docker login -u izchaki -p Doer24295548'
+                sh 'docker push izchaki/my-flask:from-jenkins-pipeline'
+                sh 'docker rmi izchaki/my-flask:from-jenkins-pipeline'
+            }
+        }
+        stage('list files') {
+            agent {
+                docker {
+                    image "moditamam/selenium:from-jenkins-pipeline"
+                    reuseNode true
+                }
+            }
+            steps {
+                sh 'echo worked!!!!!'
+            }
+        }
+    }
+}
